229e4460f9277cf7701a72ddd2f0028fbd5dc537
beistvan <beistvan@gmail.com>
2025-05-16 13:29:35 +0200

    count_digits_in_large_number.c

---
+++ count_digits_in_large_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    int d;
+    int count = 0;
+    int c;
+    
+    scanf("%d", &d);
+    
+    getchar();
+    
+    char k[1000001];
+    int index = 0;
+
+    while ((c = getchar()) != EOF && c != '\n') {
+        k[index++] = c;
+    }
+    k[index] = '\0';
+
+    for (int i = 0; i < index; i++) {
+        if (k[i] == '0' + d) {
+            count++;
+        }
+    }
+
+    printf("%d %s %d\n", d, k, count);
+    
+    return 0;
+}
