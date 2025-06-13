121bfa6752ff337b6064e5d22f371cc0b6ddf277
beistvan <beistvan@gmail.com>
2025-06-13 12:31:28 +0200

    sum-of-main-diagonal.c

---
+++ sum-of-main-diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum_diag(int a[3][3]) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += a[i][i];
+    }
+    return sum;
+}
+
+int main() {
+    int a[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("%d\n", sum_diag(a));
+
+    return 0;
+}
